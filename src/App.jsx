fb4e56fa71537cdc88ebd19051256201e03219b1
Martin Kang <martin.kang@efrei.net>
2024-04-03 17:05:49 +0200

    First init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+import NavigationFooter from "./components/NavigationFooter.jsx";
+export default function App() {
+    return (
+        <>
+            <div className="artboard phone-1 relative h-32 w-32 bg-[pink]">
+                    <NavigationFooter/>
+            </div>
+        </>
+    );
+}
---
+++ src/components/NavigationFooter.jsx
@@ -0,0 +1,37 @@
+import HomeIcon from "../assets/icons/home-icon.svg?react"
+import HeartIcon from "../assets/icons/heart-icon.svg?react"
+import CardsIcon from "../assets/icons/cards-icon.svg?react"
+import ProfileIcon from "../assets/icons/profile-icon.svg?react"
+function NavigationFooter() {
+    return(
+        <>
+            <div className="absolute inset-x-0 bottom-0 m-5">
+            <ul className="menu menu-horizontal bg-base-200 mt-6 w-full justify-evenly rounded-full">
+                <li>
+                    <a className="tooltip hover:bg-[#25304F] bg-[#25304F] rounded-full p-2.5 m-0.5 " data-tip="Home">
+                        <HomeIcon className={"fill-[#E7E7EB] hover:fill-blue-500 "}/>
+                    </a>
+                </li>
+                <li>
+                    <a className="tooltip hover:bg-[#25304F] bg-[#25304F] rounded-full p-2.5 m-0.5" data-tip="Statistics">
+                        <HeartIcon className={"fill-[#E7E7EB] hover:fill-blue-500"}/>
+                    </a>
+                </li>
+                <li>
+                    <a className="tooltip hover:bg-[#25304F] bg-[#25304F] rounded-full p-2.5 m-0.5"  data-tip="Associations">
+                        <CardsIcon className={"fill-[#E7E7EB] hover:fill-blue-500 size-10"}/>
+                    </a>
+                </li>
+                <li>
+                    <a className="tooltip hover:bg-[#25304F] bg-[#25304F] rounded-full p-2.5 m-0.5"  data-tip="Profile">
+                        <ProfileIcon className={"fill-[#E7E7EB] hover:fill-blue-500"}/>
+                    </a>
+                </li>
+            </ul>
+            </div>
+
+        </>
+    )
+}
+
+export default NavigationFooter
