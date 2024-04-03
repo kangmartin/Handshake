bdc80070d28e50b76c2d39a0e5070a7ec9d2b66a
Martin Kang <martin.kang@efrei.net>
2024-04-03 23:16:43 +0200

    Usage of own custom theme based on figma mockup

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,32 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["night", "light"],
+    themes: [
+        "night",
+      "light",
+      {
+        mytheme: {
+
+          "primary": "#0073E6",
+
+          "secondary": "#25304F",
+
+          "accent": "#f89900",
+
+          "neutral": "#101228",
+
+          "base-100": "#16193B",
+
+          "info": "#006bee",
+
+          "success": "#00ff6f",
+
+          "warning": "#ae6f00",
+
+          "error": "#ec4760",
+        },
+      },
+    ],
   },
 }
 
