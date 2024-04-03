c1a84cfd053393c97db29aa894d5799004e03695
Martin Kang <martin.kang@efrei.net>
2024-04-03 23:16:08 +0200

    Remove the artboard and start dev in full size

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import FavoriteAssociationsList from "./components/FavoriteAssociationsList.jsx"
 export default function App() {
     return (
         <>
-            <div className="artboard phone-1 relative bg-[#25304F] flex justify-center pt-5">
+            {/*<div className="artboard phone-1 relative bg-[#25304F] flex justify-center pt-5">*/}
                 <FavoriteAssociationsList/>
+
                 <NavigationFooter/>
-            </div>
+            {/*</div>*/}
         </>
     );
 }
